7e726c957786898d0dac6ba5a2162c520a993312
Favour Njoku <favour.njoku@icloud.com>
2025-07-31 22:11:51 +0100

    fix: Next js already has a built in environment for knowing if it's running in production or not

--- src/app/configs/constant.ts
+++ src/app/configs/constant.ts
@@ -1,5 +1,5 @@
 // *********************************************************//
 // *********** BASIC CONFIG CONSTANTS ***********************/
 // *********************************************************//
-export const ENVIRONMENT = process.env.NEXT_PUBLIC_ENVIRONMENT ?? "dev";
+export const ENVIRONMENT = process.env.NODE_ENV;
 export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL ?? "";
